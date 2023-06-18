bf5637ebaf8afecc11bc906c16a960264bac35cb
fredericbob <94382364+fredericbob@users.noreply.github.com>
2023-06-18 10:54:27 +0300

    Sprint 4

---
+++ TestFramework/script.bat
@@ -0,0 +1,9 @@
+cd "C:\Users\frédéric\Desktop\Web Dyn\Framework\out\production\Framework"
+jar cvf fw.jar *
+copy "./fw.jar" "C:\Users\frédéric\Desktop\Web Dyn\TestFramework\WEB-INF\lib\"
+cd "C:\Users\frédéric\Desktop\Web Dyn\TestFramework\WEB-INF\classes"
+javac -cp "C:\Users\frédéric\Desktop\Web Dyn\TestFramework\WEB-INF\lib\fw.jar" -d . *.java
+cd "C:\Users\frédéric\Desktop\Web Dyn\TestFramework"
+jar cvf "TestFramework.war" *
+copy "TestFramework.war" "C:\Program Files\Apache Software Foundation\Tomcat 10.1\webapps"
+del "TestFramework.war"
